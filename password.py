fab5d343a2c281f1ecc9bd99eb09bf763ac192aa
shrutijadhav380 <shrutijadhav380@gmail.com>
2026-05-04 21:18:50 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+import random 
+import string 
+
+lenght=int(input("enter a password lenght:"))
+char=string.ascii_letters+string.digits+string.punctuation
+
+password=""
+for i in range(lenght):
+    password+=random.choice(char)
+print("generated password:",password)
